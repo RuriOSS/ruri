c2564b8ff0b907ff82784c2b667fbd2984cabac8
dabao1955 <dabao1955@163.com>
2026-04-15 22:20:04 +0800

    fix

--- src/info.c
+++ src/info.c
@@ -149,7 +149,7 @@ void ruri_show_helps(void)
 	cprintf("{base}  -O, --oom-score-adj [score] .................: Set oom_score_adj for container (*14)\n");
 	cprintf("{base} -Q, --mask-path [path] ......................: Mask a path in the container\n");
 #ifndef DISABLE_SYSTEMD
-	cprintf("{base} -Z, --systemd .............................: Run container with systemd support (includes dbus) (*15)\n");
+	cprintf("{base} -Z, --systemd .............................: Run container with systemd support (includes dbus and keeps CAP_SYS_RESOURCE) (*15)\n");
 #endif
 	cprintf("{base} -z, --enable-tty-signals ....................: Enable TTY signals in the container (*16)\n");
 	cprintf("{base}  -g, --skip-setgroups ........................: Skip setgroups() call\n");
